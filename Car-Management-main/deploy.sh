54997d6c0bd98a988b1fef5ab03ad1cff171a943
AtharvaManchalkar <atharvamanchalkar7@gmail.com>
2025-06-01 12:29:46 +0530

    Update deployment file

--- Car-Management-main/deploy.sh
+++ Car-Management-main/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DOCKERHUB_USERNAME="yourusername"
+export DOCKERHUB_USERNAME="atharvamanchalkar"
 docker-compose -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.prod.yml down
 docker-compose -f docker-compose.prod.yml up -d
